812e86a2a59121ea67b50c98c708426d9478d943
FOZAEL <110026345+FOZAEL23@users.noreply.github.com>
2022-09-27 18:08:20 +0430

    some changes

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const { inspect } = require("util");
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const REACTION_TYPES = [
-  "+1",
-  "-1",
-  "laugh",
-  "confused",
-  "heart",
-  "hooray",
-  "rocket",
-  "eyes",
-];
 
 const HIDE_REASONS = [
   "off topic",
@@ -22,71 +12,13 @@ const HIDE_REASONS = [
   "dublicated",
 ];
 
-
-async function addReactions(octokit, repo, comment_id, reactions) {
-  let ReactionsSet = [
-    ...new Set(
-      reactions
-        .replace(/\s/g, "")
-        .split(",")
-        .filter((item) => {
-          if (!REACTION_TYPES.includes(item)) {
-            core.info(`Skipping invalid reaction '${item}'.`);
-            return false;
-          }
-          return true;
-        })
-    ),
-  ];
-
-  if (!ReactionsSet) {
-    core.setFailed(
-      `No valid reactions are contained in '${reactions}'.`
-    );
-    return false;
-  }
-
-  let results = await Promise.allSettled(
-    ReactionsSet.map(async (item) => {
-      await octokit.rest.reactions.createForIssueComment({
-        owner: repo[0],
-        repo: repo[1],
-        comment_id: comment_id,
-        content: item,
-      });
-      core.info(`Setting '${item}' reaction on comment.`);
-    })
-  );
-
-  for (let i = 0, l = results.length; i < l; i++) {
-    if (results[i].status === "fulfilled") {
-      core.info(
-        `Added reaction '${ReactionsSet[i]}' to comment id '${comment_id}'.`
-      );
-    } else if (results[i].status === "rejected") {
-      core.info(
-        `Adding reaction '${ReactionsSet[i]}' to comment id '${comment_id}' failed with ${results[i].reason}.`
-      );
-    }
-  }
-  ReactionsSet = undefined;
-  results = undefined;
-}
-
 async function run() {
   try {
     const inputs = {
       token: core.getInput("token"),
-      repository: core.getInput("repository"),
       issueNumber: core.getInput("issue-number"),
-      commentId: core.getInput("comment-id"),
       body: core.getInput("body"),
-      editMode: core.getInput("edit-mode"),
-      hideComments: core.getInput("hide-comment-contain"),
       hideReason: core.getInput("hide-reason"),
-      reactions: core.getInput("reactions")
-        ? core.getInput("reactions")
-        : core.getInput("reaction-type"),
     };
     core.debug(`Inputs: ${inspect(inputs)}`);
 
@@ -105,6 +37,7 @@ async function run() {
 
     const octokit = github.getOctokit(inputs.token);
 
+    console.log(inputs.body);
     if (inputs.body) {
       // Hide comments
       const { data: comments } = await octokit.rest.issues.listComments({
@@ -127,62 +60,6 @@ async function run() {
         }
       }
       return;
-    } else if(inputs.commentId === "test") {
-      // Edit a comment
-      if (!inputs.body && !inputs.reactions) {
-       core.setFailed("Missing either comment 'body' or 'reactions'.");
-       return;
-     }
- 
-     if (inputs.body) {
-       var commentBody = "";
-       if (editMode == "append") {
-         // Get the comment body
-         const { data: comment } = await octokit.rest.issues.getComment({
-           owner: repo[0],
-           repo: repo[1],
-           comment_id: inputs.commentId,
-         });
-         commentBody = comment.body + "\n";
-       }
- 
-       commentBody = commentBody + inputs.body;
-       core.debug(`Comment body: ${commentBody}`);
-       await octokit.rest.issues.updateComment({
-         owner: repo[0],
-         repo: repo[1],
-         comment_id: inputs.commentId,
-         body: commentBody,
-       });
-       core.info(`Updated comment id '${inputs.commentId}'.`);
-       core.setOutput("comment-id", inputs.commentId);
-     }
- 
-     // Set comment reactions
-     if (inputs.reactions) {
-       await addReactions(octokit, repo, inputs.commentId, inputs.reactions);
-     } 
-    } else if (inputs.issueNumber === "test") {
-      // Create a comment
-      if (!inputs.body) {
-        core.setFailed("Missing comment 'body'.");
-        return;
-      }
-      const { data: comment } = await octokit.rest.issues.createComment({
-        owner: repo[0],
-        repo: repo[1],
-        issue_number: inputs.issueNumber,
-        body: inputs.body,
-      });
-      core.info(
-        `Created comment id '${comment.id}' on issue '${inputs.issueNumber}'.`
-      );
-      core.setOutput("comment-id", comment.id);
-
-      // Set comment reactions
-      if (inputs.reactions) {
-        await addReactions(octokit, repo, comment.id, inputs.reactions);
-      }
     } else {
       core.setFailed("Missing either 'issue-number' or 'comment-id'.");
       return;
