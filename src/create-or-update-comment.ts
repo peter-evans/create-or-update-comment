a2b289339134e74b664ad36d34b12091a0c5ea65
Ethan Davidson <edavidson@leagueapps.com>
2023-10-18 16:38:45 -0400

    add truncate warning to body of comment

--- src/create-or-update-comment.ts
+++ src/create-or-update-comment.ts
@@ -120,9 +120,10 @@ function appendSeparatorTo(body: string, separator: string): string {
 
 function truncateBody(body: string) {
   // 65536 characters is the maximum allowed for issue comments.
+  let truncateWarning = "*Comment has been truncated*"
   if (body.length > 65536) {
     core.warning(`Comment body is too long. Truncating to 65536 characters.`)
-    return body.substring(0, 65536)
+    return body.substring(0, 65536-truncateWarning.length) + truncateWarning
   }
   return body
 }
